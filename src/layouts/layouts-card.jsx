4e062a85d8e01ae4102af72ff4003b87c500a121
gustavo <bocettodigital@gmail.com>
2021-09-27 23:38:44 -0500

    se agrearon promps

--- src/layouts/layouts-card.jsx
+++ src/layouts/layouts-card.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 
-const LayoutsCard = ({children}) => {
+const LayoutsCard = ({children, titulo, subtitulo}) => {
   return (
     <div class="main-content" style={{ minHeight: 581 }}>
       <section class="section">
         <div class="section-header">
-          <h1>"tirtulo"</h1>
+          <h1>{titulo}</h1>
           <div class="section-header-breadcrumb">
             <div class="breadcrumb-item active">
               <Link to="#">Dashboard</Link>
@@ -17,7 +17,7 @@ const LayoutsCard = ({children}) => {
           </div>
         </div>
         <div class="section-body">
-          <h2 class="section-title">2tieulo</h2>
+          <h2 class="section-title">{subtitulo}</h2>
           {/* <p class="section-lead">This page is just an example for you to create your own page.</p> */}
           <div class="card">
             <div class="card-header">{/* <h4>Example Card</h4> */}</div>
--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -2,7 +2,7 @@ import LayoutsCard from "layouts/layouts-card";
 
 const Users = () => {
   return (
-      <LayoutsCard >
+      <LayoutsCard titulo="Usuarios" subtitulo="Listado de usuarios">
         <h1>Aqui va el contenido</h1>
       </LayoutsCard>
   );
