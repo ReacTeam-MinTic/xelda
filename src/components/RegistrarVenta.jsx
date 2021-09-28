bf546a74631e34c3a8641ed6a9c168d70434d122
Andrés Felipe Pérez G <andresfe52@gmail.com>
2021-09-28 08:46:17 -0500

    se aplicó estilos al formulario de nueva venta

--- src/components/RegistrarVenta.jsx
+++ src/components/RegistrarVenta.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const RegistrarVenta = () => {
     return (
         <>
-            <form >
-                <span>Item #</span>
-                <input type="text" placeholder="Nombre del producto" className="my-3 mx-2"/>
-                <input type="number" placeholder="cantidad de items" className="my-3 mx-2"/>
-                <input type="number" placeholder="Valor unitario" className="my-3 mx-2"/>
-                <button className="btn btn-success mx-3" type="submit">Guardar</button>
-                <button className="btn btn-danger" type="reset">Eliminar</button>
+            <form className="d-flex justify-content-center align-items-center">
+                <p className="m-1">Item #</p>
+                <input type="text" placeholder="Nombre del producto" className="form-control my-3 mx-2"/>
+                <input type="number" placeholder="cantidad de items" className="form-control my-3 mx-2"/>
+                <input type="number" placeholder="Valor unitario" className="form-control my-3 mx-2"/>
+                <button className="btn btn-success rounded-pill mx-3" type="submit">Guardar</button>
+                <button className="btn btn-danger rounded-pill " type="reset">Eliminar</button>
             </form>
         </>
     )
--- src/layouts/layouts-card.jsx
+++ src/layouts/layouts-card.jsx
@@ -29,7 +29,7 @@ const LayoutsCard = ({children, titulo, subtitulo, tituloCardHeader, nombreBoton
             <div className="card-body">
               <main>{children}</main>
             </div>
-            <div className="card-footer bg-whitesmoke">This is card footer</div>
+{/*             <div className="card-footer bg-whitesmoke">This is card footer</div> */}
           </div>
         </div>
       </section>
