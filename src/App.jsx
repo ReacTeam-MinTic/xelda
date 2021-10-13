ef077a2bbedcdf2b22684a37609b976527e8f7c8
Andrés Felipe Pérez Guizaho <andresfe52@gmail.com>
2021-10-13 07:39:03 -0500

    se agregó el provider del auth0 en el app.js

--- src/App.jsx
+++ src/App.jsx
@@ -8,52 +8,55 @@ import Ventas from "pages/Ventas";
 import ListVentas from "pages/listventas";
 import AuthRegister from "pages/auth-register";
 import AppUser from "pages/formUser/appUser";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 // Un Route por cada Layouts - Ver los layouts como pages-templante (NO como componentes)
 // Agreagr otro Toute para Login
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path={["/test", "/ventas", "/listventas", "/formproducts", "/listproducts", "/users"]}>
-          <Layouts>
-            <Switch>
-              <Route path="/ventas" exact>
-                <Ventas />
-              </Route>
-              <Route path="/listventas" exact>
-                <ListVentas />
-              </Route>
-              <Route path="/users" exact>
-                <AppUser />
-              </Route>
-            </Switch>
-          </Layouts>
-        </Route>
-        <Route path={["/login", "/auth-register"]}>
-          <Login>
-            <Switch>
-              <Route path="/login" exact>
-                <LoginUser />
-              </Route>
-              <Route path="/auth-register" exact>
-                <AuthRegister />
-              </Route>
-            </Switch>
-          </Login>
-        </Route>
-        <Route path={["/"]}>
-          <Layouts>
-            <Switch>
-              <Route path="/" exact>
-                <Index />
-              </Route>
-            </Switch>
-          </Layouts>
-        </Route>
-      </Switch>
-    </Router>
+    <Auth0Provider>
+      <Router>
+        <Switch>
+          <Route path={["/test", "/ventas", "/listventas", "/formproducts", "/listproducts", "/users"]}>
+            <Layouts>
+              <Switch>
+                <Route path="/ventas" exact>
+                  <Ventas />
+                </Route>
+                <Route path="/listventas" exact>
+                  <ListVentas />
+                </Route>
+                <Route path="/users" exact>
+                  <AppUser />
+                </Route>
+              </Switch>
+            </Layouts>
+          </Route>
+          <Route path={["/login", "/auth-register"]}>
+            <Login>
+              <Switch>
+                <Route path="/login" exact>
+                  <LoginUser />
+                </Route>
+                <Route path="/auth-register" exact>
+                  <AuthRegister />
+                </Route>
+              </Switch>
+            </Login>
+          </Route>
+          <Route path={["/"]}>
+            <Layouts>
+              <Switch>
+                <Route path="/" exact>
+                  <Index />
+                </Route>
+              </Switch>
+            </Layouts>
+          </Route>
+        </Switch>
+      </Router>
+    </Auth0Provider>
   );
 };
 
