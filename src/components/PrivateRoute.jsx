70c1753dc3f857354a79280ae98e50b316fb1d56
Andrés Felipe Pérez Guizaho <andresfe52@gmail.com>
2021-10-14 22:15:37 -0500

    configuración de autorización con tokens

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,8 +1,20 @@
-import {React } from 'react'
+import {React, useEffect } from 'react'
 import { useAuth0 } from "@auth0/auth0-react";
 
 const PrivateRoute = ({ children }) => {
-    const { user, isAuthenticated, isLoading, loginWithRedirect} = useAuth0()
+    const { user, isAuthenticated, isLoading, loginWithRedirect, getAccessTokenSilently } = useAuth0()
+
+    useEffect(() => {
+        const fetchAuth0Token = async ()=>{
+            const accessToken = await getAccessTokenSilently({
+                audience: 'api-autenticacion-xelda' 
+            })
+            localStorage.setItem('token', accessToken)
+        }
+        if(isAuthenticated){
+            fetchAuth0Token()
+        }
+    }, [isAuthenticated, getAccessTokenSilently])
 
     if(isLoading) return <div><h2>Loading...</h2></div>
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,17 @@
 import axios from "axios";
 
+const getToken = ()=>{
+  return `Bearer ${localStorage.getItem('token')}`
+}
+
 export const getUsersBackend = async (setUsersDb, setRunQuery) => {
-    const options = { method: "GET", url: "http://localhost:5000/users" };
+    const options = { 
+      method: "GET",
+      url: "http://localhost:5000/users",
+      headers:{
+        Authorization: getToken() //Ponerle Authorization: getToken() en el header de cada petición
+      } 
+    };
     await axios
       .request(options)
       .then(function (response) {
