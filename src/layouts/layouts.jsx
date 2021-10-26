8d63931e70c97f884853b07ceb8d8b4e99794ad6
gustavo <bocettodigital@gmail.com>
2021-10-26 17:13:20 -0500

    app ok

--- src/layouts/layouts.jsx
+++ src/layouts/layouts.jsx
@@ -1,4 +1,4 @@
-import PrivateRoute from "components/PrivateRoute";
+
 import Footer from "components/template-base/Footer";
 import MainSidebar from "components/template-base/MainSidebar";
 import NavBar from "components/template-base/NavBar";
