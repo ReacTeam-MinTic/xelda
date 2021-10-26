e8e6d88220f27ec0990f39bece55d4b47683620d
gustavo <bocettodigital@gmail.com>
2021-10-26 16:41:39 -0500

    erroere de merge

--- src/components/auth0/privateRoute.jsx
+++ src/components/auth0/privateRoute.jsx
@@ -1,46 +1,27 @@
 import { useUser } from "context/userContext";
 import React from "react";
-import { useEffect } from "react";
-import { useAuth0 } from "@auth0/auth0-react";
-import { getUserLogin } from "utils/api";
 
 const PrivateRoute = ({ rolesList, children }) => {
-  const {
-    loginWithRedirect,
-    isAuthenticated,
-    isLoading,
-    getAccessTokenSilently,
-  } = useAuth0();
-
   const { userData } = useUser();
   if (rolesList.includes(userData.role)) {
     return children;
   }
-
-
-  useEffect(() => {
-    const fecthAuthoToken =  async () => {
-      const accessToken =  await getAccessTokenSilently({ audience: `api-xelda-auth` });
-      localStorage.setItem("Token", accessToken);
-      console.log("Token: ", accessToken)
-      await getUserLogin(
-        (response)=>{
-          console.log("Respuesta: ", response);
-          setUserData(response.data);
-        }, 
-        (err)=>{
-          console.log("Error: ", err);
-        }
-        );
-    };
-    if(isAuthenticated){
-      fecthAuthoToken();
-    }
-  }, [isAuthenticated, getAccessTokenSilently]);
-
-  if (isLoading) return <div>is loading.....</div>;
-  return isAuthenticated ? <>{children}</> : loginWithRedirect();
-  
+  return (
+    <section className="section">
+      <div className="section-body">
+        <div className="card">
+          <div className="card-header">
+            <h3 style={{color: "black"}}>¡ATENCIÓN!</h3>
+          </div>
+          <div className="card-body">
+            <h1>Por favor espere que el administrador del sistema le asigne un rol para poder ingresar a los módulos.</h1>
+            <h3>Agredecemos su paciencia</h3>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default PrivateRoute;
+
